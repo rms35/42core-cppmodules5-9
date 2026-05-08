20d74b2afda15a2c8a7e95bd8be85bc36b5d60cc
rms35 <martinsoeder35@gmail.com>
2026-05-08 20:11:14 +0200

    CPP7 ex00 terminado

---
+++ CPP7/ex00/Swpcmp.hpp
@@ -0,0 +1,28 @@
+//
+// Created by rafael on 5/8/26.
+//
+
+#ifndef SWPCMP_HPP
+#define SWPCMP_HPP
+
+template <typename T>
+void swap(T& a, T& b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+T const& min(T const& a, T const& b)
+{
+    return (a < b) ? a : b;
+}
+
+template <typename T>
+T const& max(T const& a, T const& b)
+{
+    return (a > b) ? a : b;
+}
+
+#endif //SWPCMP_HPP
